7f74dea90a8ba4c3f90039baaed14f6043ddd366
wangwenhui <wangwenhui@jiaxincloud.com>
2016-09-22 20:05:10 +0800

    修改脚本 增加while循环 以及退出序号

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
-#/bin/bash
-
-
-
+while : 
+do
 echo '**********选择更新的git项目**********'
 echo '0.all'
 echo '1.jiaxin_lib_core'
@@ -13,9 +11,13 @@ echo '6.jiaxin_gw_statistics'
 echo '7.jiaxin_gw_config'
 echo '8.jiaxin_gw_container'
 echo '9.jiaxin_gw_order'
+echo 'e.exit'
+echo '----------输入更新项目的序号-----------------'
+echo '----------退出请输入：e   -----------------'
 read project
-               #在控制台输入1 2 3，它们之间用空格隔开。
-if test $project -eq 0  ;then
+
+case $project in 
+    "0")  
 echo '------------------------jiaxin_lib_core-----------------------'
 cd jiaxin_lib_core && git pull && cd ..
 echo '------------------------jiaxin_lib_dubbox-----------------------'
@@ -34,51 +36,72 @@ echo '------------------------jiaxin_gw_container-----------------------'
 cd jiaxin_gw_container && git checkout *.jar && git pull && cd ..
 echo '------------------------jiaxin_gw_order-----------------------'
 cd jiaxin_gw_order && git pull && cd ..
-fi
-
-if test $project -eq 1 ;then   
+continue
+    ;;
+    "1") 
 echo '-----------------------jiaxin_lib_core-START-----------------------' 
 
 cd jiaxin_lib_core && git pull && cd ..
-echo '------------------------jiaxin_lib_core-END-----------------------';
-fi
-if test $project -eq 2  ;then
+echo '------------------------jiaxin_lib_core-END-----------------------'
+continue
+    ;; 
+    "2")  
 echo '------------------------jiaxin_lib_dubbox-START----------------------'
 cd jiaxin_lib_dubbox && git pull && cd ..
-echo '-----------------------jiaxin_lib_dubbox-END-----------------------';
-fi
-if test $project -eq 3  ;then
+echo '-----------------------jiaxin_lib_dubbox-END-----------------------'
+continue
+    ;;
+       "3")  
 echo '------------------------jiaxin_web_devcenter-START----------------------'
 cd jiaxin_web_devcenter && git pull && cd ..
-echo '-----------------------jiaxin_web_devcenter-END-----------------------';
-fi
-if test $project -eq 4  ;then
+echo '-----------------------jiaxin_web_devcenter-END-----------------------'
+continue
+    ;;
+       "4")  
 echo '------------------------jiaxin_web_agent-START----------------------'
 cd jiaxin_web_agent && git pull && cd ..
-echo '-----------------------jiaxin_web_agent-END-----------------------';
-fi
-if test $project -eq 5  ;then
+echo '-----------------------jiaxin_web_agent-END-----------------------'
+continue
+    ;;
+       "5") 
 echo '------------------------jiaxin_web_conf-START----------------------'
 cd jiaxin_web_conf && git pull && cd ..
-echo '-----------------------jiaxin_web_conf-END-----------------------';
-fi
-if test $project -eq 6  ;then
+echo '-----------------------jiaxin_web_conf-END-----------------------'
+continue
+    ;;
+       "6") 
 echo '------------------------jiaxin_gw_statistics-START----------------------'
 cd jiaxin_gw_statistics && git pull && cd ..
-echo '-----------------------jiaxin_gw_statistics-END-----------------------';
-fi
-if test $project -eq 7  ;then
+echo '-----------------------jiaxin_gw_statistics-END-----------------------'
+continue
+    ;;
+       "7") 
 echo '------------------------jiaxin_gw_config-START----------------------'
 cd jiaxin_gw_config && git pull && cd ..
-echo '-----------------------jiaxin_gw_config-END-----------------------';
-fi
-if test $project -eq 8  ;then
+echo '-----------------------jiaxin_gw_config-END-----------------------'
+continue
+    ;;
+       "8")  
 echo '------------------------jiaxin_gw_container-START----------------------'
 cd jiaxin_gw_container && git checkout *.jar && git pull && cd ..
-echo '-----------------------jiaxin_gw_container-END-----------------------';
-fi
-if test $project -eq 9  ;then
+echo '-----------------------jiaxin_gw_container-END-----------------------'
+continue
+    ;;
+
+       "9")  
 echo '------------------------jiaxin_gw_order-START----------------------'
 cd jiaxin_gw_order && git pull && cd ..
-echo '-----------------------jiaxin_gw_order-END-----------------------';
-fi
+echo '-----------------------jiaxin_gw_order-END-----------------------'
+continue
+    ;;
+        "e")  
+echo '------------------------THANK YOU----------------------'
+break
+    ;;
+    *)             echo "#####error input#####"
+                   echo "#####请重新输入#####"
+continue
+;;
+esac
+done
+
